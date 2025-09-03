0e0e0950f65b9580252d05699dbb62c1f3208085
ImplFerris <implferris@proton.me>
2025-09-03 07:35:11 +0530

    removing unnecessary anyhow

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,9 @@
-use anyhow::Result;
 use esp_alloc as _;
-use rtc_hal::error::Error;
+use rtc_hal::error::{Error, ErrorKind};
 use rtc_hal::square_wave::{SquareWave, SquareWaveFreq};
 use rtc_hal::{datetime::DateTime, rtc::Rtc};
+
+type Result<T> = core::result::Result<T, ErrorKind>;
 
 pub struct DemoApp<RTC> {
     rtc: RTC,
@@ -17,13 +18,11 @@ where
     }
 
     pub fn set_datetime(&mut self, dt: &DateTime) -> Result<()> {
-        self.rtc
-            .set_datetime(dt)
-            .map_err(|e| anyhow::anyhow!("RTC error: {:?}", e))?;
+        self.rtc.set_datetime(dt).map_err(|e| e.kind())?;
         Ok(())
     }
 
-    pub fn print_current_time(&mut self) -> core::result::Result<(), rtc_hal::error::ErrorKind> {
+    pub fn print_current_time(&mut self) -> Result<()> {
         let current_time = self.rtc.get_datetime().map_err(|e| e.kind())?;
 
         defmt::info!(
@@ -46,16 +45,12 @@ where
     pub fn start_square_wave(&mut self) -> Result<()> {
         self.rtc
             .start_square_wave(SquareWaveFreq::Hz1)
-            .map_err(|e| anyhow::anyhow!("RTC error: {:?}", e))?;
-
+            .map_err(|e| e.kind())?;
         Ok(())
     }
 
     pub fn stop_square_wave(&mut self) -> Result<()> {
-        self.rtc
-            .disable_square_wave()
-            .map_err(|e| anyhow::anyhow!("RTC error: {:?}", e))?;
-
+        self.rtc.disable_square_wave().map_err(|e| e.kind())?;
         Ok(())
     }
 }
